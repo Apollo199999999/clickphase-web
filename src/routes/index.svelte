d5181aefd3a9974420e724eceda9b4aa02860639
Matthias Wang <matthiaswang60@gmail.com>
2022-02-26 21:03:35 +0800

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<style>
+    :global(.dark){
+        background: #202020;
+        color: #ffffff;
+        padding: 0px;
+    }
+    :global(.light){
+        background: #F3F3F3;
+        color: #1A1A1A;
+        padding: 0px;
+    }
+</style>
