842998c06bcbdbf8a3aa6c54cd8721712f7ea417
Matthias Wang <matthiaswang60@gmail.com>
2022-02-27 20:45:57 +0800

    Implemented rainbow background

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,12 +7,41 @@
 	<title>About</title>
 </svelte:head>
 
-<div class="rainbow-background">	
+<div class="mask">
+	<div class="rainbow-background">	
 	
-	<Button>Click me!</Button>
-	<Checkbox>Check me!</Checkbox>
+	</div>
 </div>
 
+
 <style lang="scss">
-		@use "about.scss";
+	/*TODO: IMPLEMENT IMAGE MASK*/
+	.mask{
+ 		 mask: url("/ui/hero-mask.png");
+  		mask-repeat: no-repeat;    
+	}
+	@keyframes rainbow-background-hue-rotate {
+			0% {
+				filter: none;
+			}
+			50% {
+				filter: hue-rotate(180deg);
+			}
+			to {
+				filter: none;
+			}
+		}
+	.rainbow-background {
+    	background: 800% 800% /* pos */
+			linear-gradient(
+							to bottom right,
+							hsl(133, 67%, 59%),
+							hsl(172, 100%, 42%),
+							hsl(202, 97%, 45%),
+							var(--fds-solid-background-base)
+			);
+			animation: rainbow-background-hue-rotate 15s infinite linear,
+			scroll-gradient 15s infinite linear;
+		height: 400px;
+	}
 </style>
