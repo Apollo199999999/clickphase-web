01aeb90883c6492f24d4418d6861f37aac530e08
Matthias Wang <matthiaswang60@gmail.com>
2022-03-11 19:16:25 +0800

    Recreate Svelte App, half-implemented navigation bar

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,85 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from './logo-img.png';
+	import "fluent-svelte/theme.css";
+	import { TextBlock, Button } from "fluent-svelte";
+</script>
+
+<header>
+	<div class="corner">
+		<a sveltekit:prefetch href="/">
+			<img src={logo} alt="ClickPhase" />
+			<TextBlock variant="title">ClickPhase</TextBlock>
+		</a>
+	</div>
+
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'}>
+				<Button sveltekit:prefetch href="/">Home</Button>
+			</li>
+			<li class:active={$page.url.pathname === '/about'}>
+				<Button sveltekit:prefetch href="/about">About</Button>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="corner">
+		<!-- TODO put something else here? github link? -->
+	</div>
+</header>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	header {
+		display: flex;
+		background-color: var(--fds-card-background-default);
+	}
+
+	.corner {
+		height: 4em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		text-decoration: none;
+		color: inherit;
+		
+	}
+
+	.corner img {
+		width: 2.5em;
+		height: 2.5em;
+		object-fit: contain;
+		margin-right: 5px;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		margin-left: 3em;
+	}
+
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+		margin-right: 5px;
+	}
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+</style>
