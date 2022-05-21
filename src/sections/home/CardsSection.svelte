24b44ec406b456c707ee01bef27407fb08ee0cc0
Matthias Wang <matthiaswang60@gmail.com>
2022-05-21 15:25:17 +0800

    Fix Card view on mobile

--- src/sections/home/CardsSection.svelte
+++ src/sections/home/CardsSection.svelte
@@ -30,23 +30,33 @@
         flex-direction: row;
     }
     .card {
-        margin: 10px;
+        margin: 0.7rem;
         margin-top: -10em;
+        margin-bottom: 12em;
         width: 300px;
         height: 400px;
         text-align: center;
         border-radius: 3px;
         z-index: 2147483646;
+        border: red;
         -webkit-transition: 200ms ease-in-out;
         -o-transition: 200ms ease-in-out;
         transition: 200ms ease-in-out;
-        -webkit-box-shadow: 0 0 15px var(--fds-card-shadow);
-        box-shadow: 0 0 15px var(--fds-card-shadow);
-        background: linear-gradient(var(--fds-card-background-default), var(--fds-card-background-default)),
-                linear-gradient(var(--fds-solid-background-base), var(--fds-solid-background-base)); /* first bg is on top of this */
+        -webkit-box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        background: linear-gradient(
+                var(--fds-card-background-default),
+                var(--fds-card-background-default)
+            ),
+            linear-gradient(
+                var(--fds-solid-background-base),
+                var(--fds-solid-background-base)
+            ); /* first bg is on top of this */
     }
 
     .card:hover {
-        transform: scale(1.05);
+        transform: scale(1.04);
+        -webkit-box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
     }
 </style>
