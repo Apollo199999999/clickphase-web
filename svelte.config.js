e74ba164c5cbde589ae10f5e2ab0bcb52d4aa838
Matthias Wang <matthiaswang60@gmail.com>
2022-05-24 23:15:37 +0800

    switch to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
 export default {
   kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
+    // default options are shown
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false,
     }),
 
     prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
       default: true
     }
   }
