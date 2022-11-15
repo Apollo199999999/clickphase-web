3e2ba8641ddf6db3cd211a42dfc2821ee59c89d9
Matthias Wang <matthiaswang60@gmail.com>
2022-11-15 15:31:41 +0800

    Add Granny Keyboard page SEO

--- src/routes/granny-keyboard.svelte
+++ src/routes/granny-keyboard.svelte
@@ -1,16 +1,35 @@
 <script>
 	import "fluent-svelte/theme.css";
+	import SvelteSeo from "svelte-seo";
 	import HeroSection from "/src/sections/granny-keyboard/HeroSection.svelte";
 	import AboutGrannyKeyboard from "/src/sections/granny-keyboard/AboutGrannyKeyboard.svelte";
 	import DownloadLinks from "/src/sections/granny-keyboard/DownloadLinks.svelte";
 </script>
 
 <svelte:head>
-	<title>Granny Keyboard - ClickPhase</title>
-	<meta name="description" content="Granny Keyboard - Your keys will start worshipping Granny" />
-	<meta name="keywords" content="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js" />
 	<meta name="author" content="ClickPhase" />
 </svelte:head>
+
+<SvelteSeo
+	title="Granny Keyboard - ClickPhase"
+	description="Granny Keyboard - Your keys will start worshipping Granny"
+	keywords="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js"
+	canonical="https://clickphase.vercel.app/granny-keyboard"
+	openGraph={{
+		title: "Granny Keyboard - ClickPhase",
+		description: "Granny Keyboard - Your keys will start worshipping Granny",
+		url: "https://clickphase.vercel.app/granny-keyboard",
+		type: "website",
+		images: [
+			{
+				url: "https://clickphase.vercel.app/open-graph-images/GrannyKeyboardImage.png",
+				width: 1200,
+				height: 630,
+				alt: "Granny Keyboard - ClickPhase",
+			},
+		],
+	}}
+/>
 
 <HeroSection/>
 <AboutGrannyKeyboard/>
