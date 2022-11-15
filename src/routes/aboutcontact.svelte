88bbf1c43f1cbbba227295a89e768d2b6b8faf3f
Matthias Wang <matthiaswang60@gmail.com>
2022-11-15 15:01:38 +0800

    Add SEO for aboutcontact.svelte

--- src/routes/aboutcontact.svelte
+++ src/routes/aboutcontact.svelte
@@ -1,15 +1,34 @@
 <script>
 	import "fluent-svelte/theme.css";
+	import SvelteSeo from "svelte-seo";
 	import HeroSection from "/src/sections/aboutcontact/HeroSection.svelte";
 	import InfoSection from "/src/sections/aboutcontact/InfoSection.svelte";
 </script>
 
 <svelte:head>
-	<title>About & Contact - ClickPhase</title>
-	<meta name="description" content="About ClickPhase and how to contact ClickPhase." />
-	<meta name="keywords" content="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js" />
 	<meta name="author" content="ClickPhase" />
 </svelte:head>
+
+<SvelteSeo
+	title="About & Contact - ClickPhase"
+	description="About ClickPhase and how to contact ClickPhase."
+	keywords="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js"
+	canonical="https://clickphase.vercel.app/aboutcontact"
+	openGraph={{
+		title: "About & Contact - ClickPhase",
+		description: "About ClickPhase and how to contact ClickPhase.",
+		url: "https://clickphase.vercel.app/aboutcontact",
+		type: "website",
+		images: [
+			{
+				url: "https://clickphase.vercel.app/open-graph-images/ClickPhaseImage.png",
+				width: 1200,
+				height: 630,
+				alt: "About & Contact - ClickPhase",
+			},
+		],
+	}}
+/>
 
 <HeroSection/>
 <InfoSection/>
