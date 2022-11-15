5156669db63c7fafb0f3560bf0aa331b100496e3
Matthias Wang <matthiaswang60@gmail.com>
2022-11-15 14:49:01 +0800

    Update animationsvideos.svelte SEO

--- src/routes/animationsvideos.svelte
+++ src/routes/animationsvideos.svelte
@@ -1,15 +1,34 @@
 <script>
 	import "fluent-svelte/theme.css";
+	import SvelteSeo from "svelte-seo";
 	import HeroSection from "/src/sections/animationsvideos/HeroSection.svelte";
 	import VideoChannels from "/src/sections/animationsvideos/VideoChannels.svelte";
 </script>
 
 <svelte:head>
-	<title>Animations & Videos - ClickPhase</title>
-	<meta name="description" content="Animations and Videos from ClickPhase and others." />
-	<meta name="keywords" content="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js" />
 	<meta name="author" content="ClickPhase" />
 </svelte:head>
+
+<SvelteSeo
+	title="Animations & Videos - ClickPhase"
+	description="Animations and Videos from ClickPhase and others."
+	keywords="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js"
+	canonical="https://clickphase.vercel.app/animationsvideos"
+	openGraph={{
+		title: "Animations & Videos - ClickPhase",
+		description: "Animations and Videos from ClickPhase and others.",
+		url: "https://clickphase.vercel.app/animationsvideos",
+		type: "website",
+		images: [
+			{
+				url: "https://clickphase.vercel.app/ClickPhaseImage.png",
+				width: 1200,
+				height: 630,
+				alt: "Animations & Videos - ClickPhase",
+			},
+		],
+	}}
+/>
 
 <HeroSection/>
 <VideoChannels/>
