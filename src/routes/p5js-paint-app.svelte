cf9268f17836dd80f232ab7bab56a8d668ee40c9
Matthias Wang <matthiaswang60@gmail.com>
2022-11-15 15:20:27 +0800

    SEO for p5js paint app page

--- src/routes/p5js-paint-app.svelte
+++ src/routes/p5js-paint-app.svelte
@@ -1,15 +1,34 @@
 <script>
 	import "fluent-svelte/theme.css";
+	import SvelteSeo from "svelte-seo";
 	import HeroSection from "/src/sections/p5js-paint-app/HeroSection.svelte"
     import P5jsFrameSection from "/src/sections/p5js-paint-app/p5jsFrameSection.svelte";
 </script>
 
 <svelte:head>
-	<title>p5.js Paint App - ClickPhase</title>
-	<meta name="description" content="p5.js - A lightweight, online, and minimal paint app made in p5.js" />
-	<meta name="keywords" content="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js" />
 	<meta name="author" content="ClickPhase" />
 </svelte:head>
+
+<SvelteSeo
+	title="p5.js Paint App - ClickPhase"
+	description="p5.js - A lightweight, online, and minimal paint app made in p5.js"
+	keywords="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js"
+	canonical="https://clickphase.vercel.app/p5js-paint-app"
+	openGraph={{
+		title: "p5.js Paint App - ClickPhase",
+		description: "p5.js - A lightweight, online, and minimal paint app made in p5.js",
+		url: "https://clickphase.vercel.app/p5js-paint-app",
+		type: "website",
+		images: [
+			{
+				url: "https://clickphase.vercel.app/open-graph-images/p5jsPaintAppImage.png",
+				width: 1200,
+				height: 630,
+				alt: "p5.js Paint App - ClickPhase",
+			},
+		],
+	}}
+/>
 
 <HeroSection/>
 <P5jsFrameSection/>
