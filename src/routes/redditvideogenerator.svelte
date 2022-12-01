94f553d035232b5ec817f94e64ba43ce5e6d2cd6
Matthias Wang <matthiaswang60@gmail.com>
2022-12-01 20:27:40 +0800

    Add RedditVideoGenerator resources

---
+++ src/routes/redditvideogenerator.svelte
@@ -0,0 +1,30 @@
+<script>
+	import "fluent-svelte/theme.css";
+	import SvelteSeo from "svelte-seo";
+</script>
+
+<svelte:head>
+	<meta name="author" content="ClickPhase" />
+</svelte:head>
+
+<SvelteSeo
+	title="RedditVideoGenerator - ClickPhase"
+	description="RedditVideoGenerator - Generate r/AskReddit Reddit videos and upload them to YouTube automatically"
+	keywords="Microsoft, microsoft, windows, app, program, click, phase, software, launcher, launcherX, cool, cool apps, clickphase, granny keyboard, launcherx, dynawin, DynaWin, p5js, p5.js"
+	canonical="https://clickphase.vercel.app/redditvideogenerator"
+	openGraph={{
+		title: "RedditVideoGenerator - ClickPhase",
+		description: "RedditVideoGenerator - Generate r/AskReddit Reddit videos and upload them to YouTube automatically",
+		url: "https://clickphase.vercel.app/redditvideogenerator",
+		type: "website",
+		images: [
+			{
+				url: "https://clickphase.vercel.app/open-graph-images/RVGImage.png",
+				width: 1200,
+				height: 630,
+				alt: "RedditVideoGenerator - ClickPhase",
+			},
+		],
+	}}
+/>
+
